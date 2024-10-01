daec3fe4d816ac3512fcc782a8eb204fb923488c
tony942316 <grasso99@icloud.com>
2024-10-01 03:31:43 -0400

    Freetype Fixes

--- ProvingGrounds/Tests/FreeType.cpp
+++ ProvingGrounds/Tests/FreeType.cpp
@@ -37,7 +37,7 @@ namespace tests::freetype
             "uniform sampler2D tex0;\n"
             "void main()\n"
             "{\n"
-            "   FragColor = vec4(1.0f, 0.5f, 1.0f, 0.0f);\n"
+            "   FragColor = vec4(1.0, 0.0, 1.0, texture(tex0, TexCoord).r);\n"
             "}\n\0";
 
         if (glfwInit() == GLFW_FALSE)
